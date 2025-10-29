b2ec937ae12d7330089c307d5d75d0afe0e5855d
andrewarteagaa <129909778+andrewarteagaa@users.noreply.github.com>
2025-10-29 15:43:32 -0700

    Update streamlit_app.py
    adapting this so that there is nutrition information displayed each time a fruit is chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,6 +52,12 @@ if time_to_insert:
     st.success('Your Smoothie is ordered!', icon="✅")
 # New section to display smoothiefroot nutrition information 
 import requests 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+if not ingredients_list:
+  ingredients_string = ''
+
+for fruit_chosen in ingredients_list:
+  ingrdients_string += fruit_chosen + ' '
+  smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+  sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
