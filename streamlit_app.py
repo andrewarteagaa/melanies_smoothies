ad5cec2b0b39be73209a1b5508024761cca774a2
andrewarteagaa <129909778+andrewarteagaa@users.noreply.github.com>
2025-10-29 16:40:38 -0700

    Update streamlit_app.py
    Ensuring the search on column is loaded into the app

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,9 @@ st.write('The name on your Smoothie will be: ', name_on_order)
 # st.write("Your favorite fruit is:", option)
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True) # this line printed the table out to the streamlit app
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True) # this line printed the table out to the streamlit app
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
